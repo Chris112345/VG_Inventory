3dc3c532841001f05b654b1c6ab4e1e30b1fdd34
Chris <csadness1123@gmail.com>
2024-11-05 16:55:42 -0700

    Reviewing component information

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardComponent } from './card/card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'VG_Inventory';
+
+  all_cards = [
+    {name: 'Minerva', image: 'Minerva Image', count : 0},
+    {name: 'Fenrir', image: 'Genrir Image', count : 0},
+    {name: "FFR Minerva", image: 'FFR Minerva Image', count : 0},
+  ];
 }
